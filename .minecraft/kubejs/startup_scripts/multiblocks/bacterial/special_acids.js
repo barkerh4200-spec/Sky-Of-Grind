2336869ffc45731b72a3ec54b61b04a78a757b1d
thewaydev <65251658+thewaydev@users.noreply.github.com>
2025-03-16 18:48:03 -0700

    adding in the acids

---
+++ .minecraft/kubejs/startup_scripts/multiblocks/bacterial/special_acids.js
@@ -0,0 +1,16 @@
+GTCEuStartupEvents.registry('gtceu:material', event => {
+
+    event.create('isocitric_acid')
+    .fluid()
+    .color(0xd3d3d3)
+    event.create('oxalic_acid')
+    .fluid()
+    .color(0xffffff)
+    event.create('pentadecanoic_acid')
+    .fluid()
+    .color(0xd3d3d3)
+    event.create('ferulic_acid')
+    .fluid()
+    .color(0xffffff)
+    
+})
