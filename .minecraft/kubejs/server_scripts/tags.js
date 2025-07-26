53824a6dd2da248cd1e1dfd46bf8dcc1ca4329f8
Phoenixvine <82596737+Phoenixvine32908@users.noreply.github.com>
2025-07-26 18:58:30 -0400

    Adds the abilty for Quarktech and Quantum armors to work as space armors.
    Porting over the moni code to allow quarktech to work as a space suit. This is to allow night vision while space mining.

--- .minecraft/kubejs/server_scripts/tags.js
+++ .minecraft/kubejs/server_scripts/tags.js
@@ -14,6 +14,25 @@ ServerEvents.tags('item', event => {
     event.add('forge:nuggets', 'avaritia:infinity_nugget')
     event.add('forge:nuggets/infinity', 'avaritia:infinity_nugget')
 
+  const advAEArmor = [
+        "advanced_ae:quantum_helmet",
+        "advanced_ae:quantum_chestplate",
+        "advanced_ae:quantum_leggings",
+        "advanced_ae:quantum_boots"
+    ]
+    event.add("ad_astra:space_resistant_armor", advAEArmor)
+    event.add("ad_astra:freeze_resistant_armor", advAEArmor)
+    event.add("ad_astra:heat_resistant_armor", advAEArmor)
 
+        const quarkTechArmor = [
+        "gtceu:quarktech_helmet",
+        "gtceu:quarktech_chestplate",
+        "gtceu:advanced_quarktech_chestplate",
+        "gtceu:quarktech_leggings",
+        "gtceu:quarktech_boots"
+    ]
+    event.add("ad_astra:space_resistant_armor", quarkTechArmor)
+    event.add("ad_astra:freeze_resistant_armor", quarkTechArmor)
+    event.add("ad_astra:heat_resistant_armor", quarkTechArmor)
 
 })
